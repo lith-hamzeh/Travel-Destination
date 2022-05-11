2ed57a1eda345959fe27d1c978ca8b773e05c5ca
lithhalim <103363189+lithhalim@users.noreply.github.com>
2022-05-11 18:51:08 +0300

    55

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Footer from "../footer/Footer"
-import Header from "../header/Header"
 import Tours from "../tours/Tours";
 const Home=()=>{
     return(
         <>
-       <Header/>
        <Tours/>
-       <Footer/>
        </>
     )
 }
